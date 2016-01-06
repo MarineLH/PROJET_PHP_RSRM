e351eb1c2b5af2ca6698a8927e5d37fb2b0a2d5e
RValeye <romain.valeye@gmail.com>
2016-01-06 09:24:10 +0100

    site
    prout

---
+++ projetphpmysql/index.php
@@ -0,0 +1,45 @@
+<!DOCTYPE HTML>
+<html>
+    <head>
+        <meta charset= "utf-8" />
+        <link rel = "stylesheet" href="css/index.css"/>
+        <link rel = "stylesheet" href="css/global.css"/>
+        <title> Accueil </title>
+    </head>
+    <script src="script/global.js"></script>
+    <body>
+        <div class="contenu">
+            <header>
+                <div class="top">
+                    <img href="index.php" src="images/logofinal.png" width="200px" height="200px"/>
+                    <h1 id ="slogan">Comp Foundation<br/>Connecting people</h1>
+                </div>
+                <div class="nav">
+                    <ul id="menu">
+                        <li><a href="index.php">Accueil</a>
+                        <li><a>Inscription</a>
+                            <ul>
+                                <li><a href="insc-i.php">Intervenant</a></li>
+                                <li><a href="insc-o.php">Organisme</a></li>
+                            </ul>
+                        </li>
+                        <li><a>Recherche</a>
+                            <ul>
+                                <li><a href="rech-i.php">Intervenant</a></li>
+                                <li><a href="rech-o.php">Organisme</a></li>
+                            </ul>
+                        </li>
+                        <li><a href="contact.php">Contact</a>
+                    </ul>
+                </div>
+            </header>
+            <div class="wrapper">
+                Salut je vais bien
+                <div class="push"></div>
+            </div>
+        </div>   
+        <div class="footer">
+            <a href="" onclick="funcMentionsLegales()">Mentions légales</a> --- Copyright &#169 Comp Foundation - 2015
+        </div>
+    </body>
+</html>
