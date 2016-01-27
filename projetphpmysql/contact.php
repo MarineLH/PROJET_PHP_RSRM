30e06a3c3523036539a240449b988c85871c403f
MarineLH <marine.lhuillier@epsi.fr>
2016-01-27 09:24:15 +0100

    pages finies

--- projetphpmysql/contact.php
+++ projetphpmysql/contact.php
@@ -4,6 +4,7 @@
         <meta charset= "utf-8" />
         <link rel = "stylesheet" href="css/index.css"/>
         <link rel = "stylesheet" href="css/global.css"/>
+		<link rel = "stylesheet" href="css/contact.css"/>
         <title> Contact </title>
     </head>
     <script src="script/global.js"></script>
@@ -12,7 +13,7 @@
             <header>
                 <div class="top">
                     <img href="index.php" src="images/logofinal.png" width="200px" height="200px"/>
-                    <h1 id ="slogan">Comp Foundation<br/>Connecting people</h1>
+                    <h1 id ="slogan">Comp Foundation<br/>Opportunity, Quality, Achievement</h1>
                 </div>
                 <div class="nav">
                     <ul id="menu">
@@ -34,7 +35,43 @@
                 </div>
             </header>
             <div class="wrapper">
-                Salut je vais bien
+				<div class="contact">
+					<form id="contact" method="post" action="traitement_formulaire.php">
+						<fieldset><legend>Vos coordonnées</legend>
+                    
+                            
+                                <p><label for="nom">Nom :</label><input type="text" id="nom" name="nom" tabindex="1" /></p>
+                            
+                            
+                           
+                                <p><label for="nom">Prenom :</label><input type="text" id="prenom" name="prenom" tabindex="2" /></p>
+                            
+                              
+                            
+                                <p><label for="email">Email :</label><input type="text" id="email" name="email" tabindex="3" /></p>
+                            
+                            
+                        </fieldset>
+                        
+                        
+						  <fieldset><legend>Votre message :</legend>
+						      <p><label for="objet">Objet :</label><input type="text" id="objet" name="objet" tabindex="4" /></p>
+						  <p><label for="message">Message :</label><textarea id="message" name="message" tabindex="5" cols="30" rows="8"></textarea></p>
+						  </fieldset>
+                        
+                        
+						<div style="text-align:center;"><input type="submit" name="envoi" value="Envoyer le formulaire !" /></div>
+					</form>
+				</div>
+			
+			
+			
+			
+			
+			
+			
+			
+			
                 <div class="push"></div>
             </div>
         </div>   
